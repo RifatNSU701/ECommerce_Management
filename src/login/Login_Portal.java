e244e48142e2cff96128db6cb164ceafec05c19f
Rifat Khan <md.khanrabbi@northsouth.edu>
2024-05-06 12:22:09 +0600

    First Commit

---
+++ src/login/Login_Portal.java
@@ -0,0 +1,12 @@
+package login;
+/**
+ *
+ * @author Rifat
+ */
+public class Login_Portal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
