a807611c520a4b89ca659b6050a63a63b98f6152
Rifat Khan <md.khanrabbi@northsouth.edu>
2024-05-08 18:34:51 +0600

    2nd Commit

--- src/login/Login_Portal.java
+++ src/login/Login_Portal.java
@@ -1,12 +1,69 @@
 package login;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  *
  * @author Rifat
  */
 public class Login_Portal {
 
+   static HashMap<String, String> users = new HashMap<String, String>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        while (choice != 3) {
+            System.out.println("Select an option: ");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Quit");
+            System.out.print("\tEnter :: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    registerUser(scanner);
+                    break;
+                case 2:
+                    loginUser(scanner);
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+        scanner.close();
+    }
+
+    static void registerUser(Scanner scanner) {
+        System.out.println("Enter your username:");
+        String username = scanner.nextLine();
+        System.out.println("Enter your password:");
+        String password = scanner.nextLine();
+        if (users.containsKey(username)) {
+            System.out.println("User already exists.");
+        } else {
+            users.put(username, password);
+            System.out.println("Registration successful.");
+        }
+    }
+
+    static void loginUser(Scanner scanner) {
+        System.out.println("Enter your username:");
+        String username = scanner.nextLine();
+        System.out.println("Enter your password:");
+        String password = scanner.nextLine();
+        if (!users.containsKey(username)) {
+            System.out.println("Invalid username or password.");
+        } else {
+            if (users.get(username).equals(password)) {
+                System.out.println("Login successful.");
+            } else {
+                System.out.println("Invalid username or password.");
+            }
+        }
     }
     
 }
